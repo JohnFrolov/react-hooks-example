ead49329d9d08f616bf67b4d3c0c6006f704045d
John Frolov <johnwfrolov@gmail.com>
2018-12-18 01:40:37 +0300

    React app with hooks.

--- src/HookApp.jsx
+++ src/HookApp.jsx
@@ -3,44 +3,19 @@ import {useState, useEffect} from "react";
 import "./style";
 
 export const HookApp = (props) => {
-    const [value, setValue] =  useState(props.value);
-    const handleOnChange = (e) => setValue(e.target.value);
-
-    const [result, setResult] =  useState('');
-
-    //componentDidMount & componentDidUpdate
-    useEffect(() => {
-        const calculateResult = () => (
-            value
-                .split("")
-                .filter(e => Number.parseInt(e, 10) >= 0)
-                .join(", ")
-        )
-    
-        setResult(calculateResult());
-    }, [value]);
-
-    const [tick, setTick] =  useState(0);
-    
-    //componentDidMount & componentWillUnmount
-    useEffect(() => {
-        const handleTick = () => setTick(prevTick => prevTick + 1);
-        const timeout = setInterval(
-            handleTick,
-            1000
-        );
-        return () =>  clearInterval(timeout);
-    }, [tick]);
+    const [value, setValue] = useFormValue(props.value);
+    const result = useResult(value);
+    const tick = useInterval(0, 1000);
 
     return (
         <div className="container">
-            <span className="title">Hook App</span>
+            <span className="title">Custom Hook App</span>
             <div className="textField">
                 <label for="someText">Enter some text</label>
                 <input
                     id="someText"
                     type="text"
-                    onChange={handleOnChange}
+                    onChange={setValue}
                     value={value}
                 />
             </div>
@@ -54,4 +29,41 @@ export const HookApp = (props) => {
             </div>      
         </div>
     )
+}
+
+function useResult(value) {
+    const [result, setResult] = useState("");
+
+    useEffect(() => {
+        setResult(value
+            .split("")
+            .filter(e => Number.parseInt(e, 10) >= 0)
+            .join(", ")
+        )
+    }, [value]);
+
+    return result
+}
+
+function useFormValue(initialValue) {
+    const [ value, setValue ] = useState(initialValue);
+    const handler = (e) =>  setValue(e.target.value);
+
+    return [
+        value,
+        handler
+    ]
+}
+
+function useInterval(start, tickTimeout) {
+    const [ tick, setTick ] = useState(start); 
+
+    useEffect(() => {
+        const handleTick = () => setTick(tick => tick + 1);
+        const timeout = setInterval(handleTick, tickTimeout);
+        
+        return () => clearInterval(timeout);
+    }, [tick]);
+
+    return tick;
 }
